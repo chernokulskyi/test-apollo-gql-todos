fbb21e39a1379f91f36090d3c88b21762c29a89a
Anton Chenokulskyi <chernokulskyi@gmail.com>
2022-08-22 16:28:11 +0300

    add code

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { ChakraProvider } from '@chakra-ui/react';
+import { ApolloProvider } from '@apollo/client';
+import client from './apollo/client';
+
+import App from './App';
+import './index.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <React.StrictMode>
+    <ApolloProvider client={client}>
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
+    </ApolloProvider>
+  </React.StrictMode>
+);
